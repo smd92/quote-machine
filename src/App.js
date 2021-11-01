310c373355bad9c3ef83d66657a382eeb1c21f4c
smd92 <69214590+smd92@users.noreply.github.com>
2021-11-01 21:54:44 +0100

    get data from Quotes API

--- src/App.js
+++ src/App.js
@@ -4,11 +4,37 @@ import React from 'react';
 class QuoteMachine extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      quote: ""
+    }
+  }
+
+  componentDidMount() {
+    const data = null;
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+      }
+    });
+    
+    xhr.open("GET", "https://yusufnb-quotes-v1.p.rapidapi.com/widget/~einstein.json");
+    xhr.setRequestHeader("x-rapidapi-key", "SIGN-UP-FOR-KEY");
+    xhr.setRequestHeader("x-rapidapi-host", "yusufnb-quotes-v1.p.rapidapi.com");
+    
+    xhr.send(data);
+    this.setState({
+      quote: xhr.response
+    });
+    console.log(xhr.response);
   }
 
   render() {
     return (
-        <QuoteBox />
+      <QuoteBox />
     );
   }
 }
