810a5cd38a2d0841f28911dacfe03e683cff9ce7
smd92 <69214590+smd92@users.noreply.github.com>
2021-11-07 21:37:50 +0100

    flexbox

--- src/App.js
+++ src/App.js
@@ -46,14 +46,12 @@ const QuoteBox = (props) => {
   return (
     <div id="quote-box">
       <p id="text">{props.text}</p>
-      <div id="author-and-tweet">
         <p id="author">{props.author}</p>
         <TwitterShareButton
         id="tweet"
         url={"\n"}
         options={{ text: `${props.text}\n${props.author}` }}
         />
-      </div>
       <button
         id="new-quote"
         className="btn btn-primary"
