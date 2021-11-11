719e4660080b69b22f4a70f98f198df3b26bc72e
smd92 <69214590+smd92@users.noreply.github.com>
2021-11-11 22:20:31 +0100

    conditional rerender

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import React from "react";
 import { TwitterShareButton } from "react-twitter-embed";
 
-class QuoteMachine extends React.Component {
+/*class QuoteMachine extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -59,6 +59,68 @@ const QuoteBox = (props) => {
         id="new-quote"
         className="btn btn-primary"
         onClick={props.fetchQuote}
+      >
+        new quote
+      </button>
+    </div>
+  );
+};*/
+
+class QuoteMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      author: "",
+      fetched: false,
+    };
+    this.fetchQuote = this.fetchQuote.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchQuote();
+  }
+
+  fetchQuote() {
+    this.setState({
+      fetched: false,
+    });
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          text: data.content,
+          author: data.author,
+          fetched: true,
+        });
+      });
+  }
+
+  render() {
+    return (
+      this.state.fetched && (
+        <div>
+          <TwitterShareButton
+            id="tweet"
+            url={"\n"}
+            options={{ text: `${this.state.text}\n${this.state.author}` }}
+          />
+          <noRerender />
+        </div>
+      )
+    );
+  }
+}
+
+const noRerender = (props) => {
+  return (
+    <div id="quote-box">
+      <p id="text">{props.text}</p>
+      <p id="author">{props.author}</p>
+      <button
+        id="new-quote"
+        className="btn btn-primary"
+        onClick={this.fetchQuote}
       >
         new quote
       </button>
